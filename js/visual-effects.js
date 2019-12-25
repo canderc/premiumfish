fd4e78ecc09b8aec27c1267b3f3fa5c06a8b2227
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-12-25 22:49:02 +0200

    Add smooth scrolling

--- js/visual-effects.js
+++ js/visual-effects.js
@@ -1,13 +1,36 @@
-const fadeInElementsList = document.getElementsByClassName('fade-in-hidden');
+
 const banner = document.getElementsByClassName('banner')[0];
+const header = document.getElementsByTagName('header')[0];
+const sections = document.getElementsByTagName('section');
 
-// @to do: use separate js file for such things
-banner.style.height = window.innerHeight + 'px';
+const setElementsHeight = () => {
+  Array.from(sections).forEach(section => { 
+    const windowHeight = window.innerHeight
+    const sectionHeight = section.getBoundingClientRect().height
+  
+    section.style.paddingTop = header.getBoundingClientRect().height + 15 + 'px'
+  
+    if (sectionHeight < windowHeight) {
+      section.style.height = window.innerHeight + 'px'
+    }
+  })
+  
+  banner.style.height = window.innerHeight + 'px';
+}
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    entry.isIntersecting  && entry.target.classList.add('fade-in')
-  }, { threshold: 1.0})
-})
+const scroll = (e) => {
+  const hash = e.target.getAttribute('href')
+  const targetClassName = hash.replace(/#/, '.')
 
-Array.from(fadeInElementsList).forEach(fadeInElement => observer.observe(fadeInElement))
+  e.preventDefault();
+  
+  window.location.hash = hash
+  document.querySelector(targetClassName).scrollIntoView({ behavior: 'smooth' })
+}
+
+document.querySelector('.nav').addEventListener('click', scroll)
+document.querySelector('.hamburg-menu_nav').addEventListener('click', scroll)
+
+setElementsHeight();
+
+window.subscribeOnResize(setElementsHeight)
