34611a61344512150f960cd76a874a7f67702cc6
canderc <49619380+canderc@users.noreply.github.com>
2019-12-30 18:01:50 +0200

    created a branch of products

---
+++ js/products.js
@@ -0,0 +1,9 @@
+const obg = {
+    title: "Корюшка",
+    discr: "Супер вкусная рыба",
+    price: "650 грн"
+};
+
+function cardProduct {
+   let sectionProduct = document.getElementsByClassName('products')
+}
