4c2c9df5a63c40b8a524e46682acc0fc4daa8a74
Oleksandr Sakun <sakun.olesk@gmail.com>
2020-01-04 23:09:45 +0200

    :art: Improve code for products rendering

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const hamburger = require('./hamburger')
-const testFetchData = require('./test-fetch')
+// const testFetchData = require('./test-fetch')
 const visualEffects = require('./visual-effects')
+require('./products')
 
 window.onload = function() {
   hamburger.onLoad && hamburger.onLoad();
-  testFetchData.onLoad && testFetchData.onLoad();
+  // testFetchData.onLoad && testFetchData.onLoad();
   visualEffects.onLoad && visualEffects.onLoad();
 }
--- js/products.js
+++ js/products.js
@@ -6,38 +6,53 @@ const productsMock = [
     { title: 'САЗАН', description: 'Эта рыба отличается темно-желто-золотистым оттенком', price: 10, category: 'fish' },
 ]
 
-let wrapCard = document.createElement('div');
-let title = document.createElement('h3');
-let description = document.createElement('span');
-let price = document.createElement ('span');
-let wrapButton = document.createElement('div');
-let buttonLook = document.createElement('button');
-let buttonBasket = document.createElement('button');
+// let wrapCard = document.createElement('div');
+// let title = document.createElement('h3');
+// let description = document.createElement('span');
+// let price = document.createElement ('span');
+// let wrapButton = document.createElement('div');
+// let buttonLook = document.createElement('button');
+// let buttonBasket = document.createElement('button');
 
-wrapCard.className = "product-wrap";
-title.className = "product-title";
-description.className = "product-discription";
-price.className = "product-price";
-wrapButton.className = "button-wrap";
-buttonLook.className = "button-look";
-buttonBasket.className = "button-basket";
+// wrapCard.className = "product-wrap";
+// title.className = "product-title";
+// description.className = "product-discription";
+// price.className = "product-price";
+// wrapButton.className = "button-wrap";
+// buttonLook.className = "button-look";
+// buttonBasket.className = "button-basket";
 
-function handleProduct (products) {
-    for (let i =0; i < products.length; i++) {
+function handleProducts (products) {
+    for (let i = 0; i < products.length; i++) {
         const product = products[i];
+
         if (product.category === 'fish') {
             drawFish(product)
         } else if (product.category === 'caviar') {
             drawCaviar(product)
-        } else {
+        } else if (product.category === 'snacks') {
             drawSnacks(product)
         }
     };
 }
-    
-function drawFish (product) {
-    let fishCategory = document.getElementsByClassName('fish-category');
-    fishCategory.append(wrapCard);
+
+function createCard (product) {
+    const wrapCard = document.createElement('div');
+    const title = document.createElement('h3');
+    const description = document.createElement('span');
+    const price = document.createElement ('span');
+    const wrapButton = document.createElement('div');
+    const buttonLook = document.createElement('button');
+    const buttonBasket = document.createElement('button');
+
+    wrapCard.className = "product-wrap";
+    title.className = "product-title";
+    description.className = "product-discription";
+    price.className = "product-price";
+    wrapButton.className = "button-wrap";
+    buttonLook.className = "button-look";
+    buttonBasket.className = "button-basket";
+
     wrapCard.append(title);
     wrapCard.append(description);
     wrapCard.append(price);
@@ -48,15 +63,37 @@ function drawFish (product) {
     title.innerHTML = product.title;
     description.innerHTML = product.description;
     price.innerHTML = product.price;
+
+    return wrapCard;
+}
+    
+function drawFish (product) {
+    const fishCategory = document.querySelector('.fish-category');
+
+    const card = createCard(product);
+
+    fishCategory.append(card);
+
+    // fishCategory.append(wrapCard);
+    // wrapCard.append(title);
+    // wrapCard.append(description);
+    // wrapCard.append(price);
+    // wrapCard.append(wrapButton);
+    // wrapButton.append(buttonLook);
+    // wrapButton.append(buttonBasket);
+
+    // title.innerHTML = product.title;
+    // description.innerHTML = product.description;
+    // price.innerHTML = product.price;
 }
 
 function drawCaviar (product) {
-    let caviarCategory = document.getElementsByClassName('caviar-category');
+    const caviarCategory = document.querySelector('.caviar-category');
 }
 
 function drawSnacks (product) {
-    let snacksCategory = document.getElementsByClassName('snacks-category');
+    const snacksCategory = document.querySelector('.snacks-category');
 }
 
-
+handleProducts(productsMock);
     
