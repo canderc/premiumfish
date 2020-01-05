140660e049e6e46e3e1f6a7a3edd69c6584b7559
canderc <49619380+canderc@users.noreply.github.com>
2020-01-05 15:33:43 +0200

    add style for mobile

--- js/products.js
+++ js/products.js
@@ -1,9 +1,14 @@
 const productsMock = [
-    { title: 'СУДАК', description: 'Судак, как и окунь, предпочитает лишь чистую воду, насыщенную кислородом и способствующую нормальной жизнедеятельности рыбы', price: 280, category: 'fish' },
+    { title: 'СУДАК', description: 'Судак, как и окунь, предпочитает лишь чистую воду, насыщенную кислородом и способствующую нормальной жизнедеятельности рыбы', price: 'Цена: 280 грн', category: 'fish' },
     { title: 'БЕРШ', description: 'Берш, как и судак, считается родственником окуня.', price: 100, category: 'fish' },
     { title: 'ОКУНЬ', description: 'Окунь предпочитает водоемы с чистой водой. Это могут быть реки, пруды, озера, водохранилища и т.д. Окунь является самым часто встречающимся хищником, но его никогда не найдешь там, где вода мутная и грязная. Для ловли окуня применяют довольно-таки тонкие снасти. Его ловля очень интересна и занимательная.', price: 630, category: 'fish' },
     { title: 'ГУСТЕРА', description: 'Это малоподвижный вид рыбы с голубовато-серым окрасом. Живет густера примерно 15 лет и врастает в длину до 35 см, при весе 1,2 кг. Густера, как и лещ, растет довольно медленно.', price: 12000, category: 'fish' },
     { title: 'САЗАН', description: 'Эта рыба отличается темно-желто-золотистым оттенком', price: 10, category: 'fish' },
+    { title: 'Икра лемонемы', description: 'Очень вкусная икра в ястыке', price: 'Цена: 700 грн', category: 'caviar'},
+    { title: 'Икра наваги', description: 'Очень вкусная икра в ястыке', price: 'Цена: 680 грн', category: 'caviar'},
+    { title: 'Палочки горбуши', description: 'Суперовые палочки горбуши фасовка по 0,5кг', price: 'Цена: 560 грн', category: 'snacks'},
+    { title: 'Икра лосося', description: 'Очень вкусная икра в ястыке', price: 'Цена: 750 грн', category: 'caviar'},
+    { title: 'Рыбные палочки', description: 'Суперовые палочки горбуши фасовка по 0,5кг', price: 'Цена 490 грн', category: 'snacks'},
 ]
 
 // let wrapCard = document.createElement('div');
@@ -57,12 +62,12 @@ function createCard (product) {
     wrapCard.append(description);
     wrapCard.append(price);
     wrapCard.append(wrapButton);
-    wrapButton.append(buttonLook);
     wrapButton.append(buttonBasket);
 
     title.innerHTML = product.title;
     description.innerHTML = product.description;
     price.innerHTML = product.price;
+    buttonBasket.innerHTML = "В корзину"
 
     return wrapCard;
 }
@@ -89,10 +94,18 @@ function drawFish (product) {
 
 function drawCaviar (product) {
     const caviarCategory = document.querySelector('.caviar-category');
+    
+    const card = createCard(product);
+
+    caviarCategory.append(card);
 }
 
 function drawSnacks (product) {
     const snacksCategory = document.querySelector('.snacks-category');
+    
+    const card = createCard(product);
+
+    snacksCategory.append(card);
 }
 
 handleProducts(productsMock);
