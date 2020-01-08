7d86a34a617a50734dca4153254a5686e882ea39
canderc <49619380+canderc@users.noreply.github.com>
2020-01-08 17:56:30 +0200

    add events for buttons + and -

--- js/products.js
+++ js/products.js
@@ -1,16 +1,16 @@
-// const productsMock = [
-//     { avatar: 'img/title_img/5.png', title: 'СУДАК', description: 'Судак, как и окунь, предпочитает лишь чистую воду, насыщенную кислородом и способствующую нормальной жизнедеятельности рыбы', price: 'Цена: 280 грн', category: 'fish' },
-//     { avatar: 'img/title_img/5.png', title: 'БЕРШ', description: 'Берш, как и судак, считается родственником окуня.', price: 100, category: 'fish' },
-//     { avatar: 'img/title_img/5.png', title: 'ОКУНЬ', description: 'Окунь предпочитает водоемы с чистой водой. Это могут быть реки, пруды, озера, водохранилища и т.д. Окунь является самым часто встречающимся хищником, но его никогда не найдешь там, где вода мутная и грязная.', price: 630, category: 'fish' },
-//     { avatar: 'img/title_img/5.png', title: 'ГУСТЕРА', description: 'Это малоподвижный вид рыбы с голубовато-серым окрасом. Живет густера примерно 15 лет и врастает в длину до 35 см, при весе 1,2 кг. Густера, как и лещ, растет довольно медленно.', price: 12000, category: 'fish' },
-//     { avatar: 'img/title_img/5.png', title: 'САЗАН', description: 'Эта рыба отличается темно-желто-золотистым оттенком', price: 10, category: 'fish' },
-//     { avatar: 'img/title_img/5.png', title: 'Икра лемонемы', description: 'Очень вкусная икра в ястыке', price: 'Цена: 700 грн', category: 'caviar'},
-//     { avatar: 'img/title_img/5.png', title: 'Икра наваги', description: 'Очень вкусная икра в ястыке', price: 'Цена: 680 грн', category: 'caviar'},
-//     { avatar: 'img/title_img/5.png', title: 'Палочки горбуши', description: 'Суперовые палочки горбуши фасовка по 0,5кг', price: 'Цена: 560 грн', category: 'snacks'},
-//     { avatar: 'img/title_img/5.png', title: 'Икра лосося', description: 'Очень вкусная икра в ястыке', price: 'Цена: 750 грн', category: 'caviar'},
-//     { avatar: 'img/title_img/5.png', title: 'Икра лосося', description: 'Очень вкусная икра в ястыке', price: 'Цена: 750 грн', category: 'caviar'},
-//     { avatar: 'img/title_img/5.png', title: 'Рыбные палочки', description: 'Суперовые палочки горбуши фасовка по 0,5кг', price: 'Цена 490 грн', category: 'snacks'},
-// ]
+const productsMock = [
+    { avatar: 'img/title_img/5.png', title: 'СУДАК', description: 'Судак, как и окунь, предпочитает лишь чистую воду, насыщенную кислородом и способствующую нормальной жизнедеятельности рыбы', price: 'Цена: 280 грн', category: 'fish' },
+    { avatar: 'img/title_img/5.png', title: 'БЕРШ', description: 'Берш, как и судак, считается родственником окуня.', price: 100, category: 'fish' },
+    { avatar: 'img/title_img/5.png', title: 'ОКУНЬ', description: 'Окунь предпочитает водоемы с чистой водой. Это могут быть реки, пруды, озера, водохранилища и т.д. Окунь является самым часто встречающимся хищником, но его никогда не найдешь там, где вода мутная и грязная.', price: 630, category: 'fish' },
+    { avatar: 'img/title_img/5.png', title: 'ГУСТЕРА', description: 'Это малоподвижный вид рыбы с голубовато-серым окрасом. Живет густера примерно 15 лет и врастает в длину до 35 см, при весе 1,2 кг. Густера, как и лещ, растет довольно медленно.', price: 12000, category: 'fish' },
+    { avatar: 'img/title_img/5.png', title: 'САЗАН', description: 'Эта рыба отличается темно-желто-золотистым оттенком', price: 10, category: 'fish' },
+    { avatar: 'img/title_img/5.png', title: 'Икра лемонемы', description: 'Очень вкусная икра в ястыке', price: 'Цена: 700 грн', category: 'caviar'},
+    { avatar: 'img/title_img/5.png', title: 'Икра наваги', description: 'Очень вкусная икра в ястыке', price: 'Цена: 680 грн', category: 'caviar'},
+    { avatar: 'img/title_img/5.png', title: 'Палочки горбуши', description: 'Суперовые палочки горбуши фасовка по 0,5кг', price: 'Цена: 560 грн', category: 'snacks'},
+    { avatar: 'img/title_img/5.png', title: 'Икра лосося', description: 'Очень вкусная икра в ястыке', price: 'Цена: 750 грн', category: 'caviar'},
+    { avatar: 'img/title_img/5.png', title: 'Икра лосося', description: 'Очень вкусная икра в ястыке', price: 'Цена: 750 грн', category: 'caviar'},
+    { avatar: 'img/title_img/5.png', title: 'Рыбные палочки', description: 'Суперовые палочки горбуши фасовка по 0,5кг', price: 'Цена 490 грн', category: 'snacks'},
+]
 
 const request = require('./services/request')
 
@@ -29,7 +29,11 @@ function handleProducts (products) {
     };
 }
 
-function createCard (product) {
+function handlePlusClick() {
+
+}
+
+function createCard (product, option) {
     const wrapCard = document.createElement('div');
     const title = document.createElement('h3');
     const description = document.createElement('span');
@@ -75,6 +79,19 @@ function createCard (product) {
     buttonBasket.innerHTML = "В корзину";
     spanPlus.innerHTML = "+";
     spanMinus.innerHTML = "-";
+    input.value = option.quantity;
+    
+    spanPlus.addEventListener ('click', function () {
+        const nowValue = input.value;
+        const newValue = parseFloat(nowValue) + option.step;
+        input.value = newValue; 
+    });
+
+    spanMinus.addEventListener ('click', function () {
+        const nowValue = input.value;
+        const newValue = parseFloat(nowValue) - option.step;
+        input.value = newValue < option.quantity ? option.quantity : newValue;
+    });
 
     return wrapCard;
 }
@@ -82,7 +99,7 @@ function createCard (product) {
 function drawFish (product) {
     const fishCategoryWrap = document.querySelector('.fish-category_wrap');
 
-    const card = createCard(product);
+    const card = createCard(product, {quantity: 1, step: 1});
 
     fishCategoryWrap.append(card);
 }
@@ -90,7 +107,7 @@ function drawFish (product) {
 function drawCaviar (product) {
     const caviarCategoryWrap = document.querySelector('.caviar-category_wrap');
     
-    const card = createCard(product);
+    const card = createCard(product, {quantity: 0.5, step: 0.5});
 
     caviarCategoryWrap.append(card);
 }
@@ -98,19 +115,19 @@ function drawCaviar (product) {
 function drawSnacks (product) {
     const snacksCategoryWrap = document.querySelector('.snacks-category_wrap');
     
-    const card = createCard(product);
+    const card = createCard(product, {quantity: 0.5, step: 0.5});
 
     snacksCategoryWrap.append(card);
 }
 
-module.exports = {
-    onLoad: function() {
-      request.get('/api/products')
-        .then(function(products) {
-          handleProducts(products)
-        });
-    }
-  }
+// module.exports = {
+//     onLoad: function() {
+//       request.get('/api/products')
+//         .then(function(products) {
+//           handleProducts(products)
+//         });
+//     }
+//   }
 
-// handleProducts(productsMock);
+handleProducts(productsMock);
     
