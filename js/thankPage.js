77062e9febb7374c457d16e0afa159ce08840199
canderc <49619380+canderc@users.noreply.github.com>
2020-02-03 09:17:46 +0200

    1

--- js/thankPage.js
+++ js/thankPage.js
@@ -2,7 +2,7 @@ const modalWindow = document.querySelector ('#window-thank');
 const thankPageButton = document.querySelector ('.thank-page_button');
 
 thankPageButton.addEventListener ('click', function() {
-    modalWindow.classList.toggle ('modal-window-close');
+    modalWindow.classList.toggle ('hidden');
     modalWindow.classList.remove ('modal-window');
 })
 
